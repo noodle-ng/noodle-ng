fa1cdfcccebcde3048edbe1001fbd397f82c31ce
Christian Hundt <hundt.christian@googlemail.com>
2010-08-28 21:08:54 +0000

    deleted unnecessary import (multiprocessing.Process) from hcrawler.py
    git-svn-id: https://noodle-ng.googlecode.com/svn/trunk@20 8aa6f4f4-6411-944d-c798-68ba1b83f409

--- hcrawler.py
+++ hcrawler.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from multiprocessing import Pool, Process
+from multiprocessing import Pool
 import time
 import logging
 from datetime import datetime, timedelta
