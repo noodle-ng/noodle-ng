e97bf1a73aec708a25ccc6fa274476eb16161d18
Christian Hundt <hundt.christian@googlemail.com>
2010-09-08 19:16:38 +0000

    added ip2int,int2ip,prettysize and more to utils.py -- further to be removed in noodle/models/share.py
    git-svn-id: https://noodle-ng.googlecode.com/svn/trunk@28 8aa6f4f4-6411-944d-c798-68ba1b83f409

--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
 import socket as sk
 
 ############################################################
-# some tools 
+# some tools to ensure the dry principle 
 ############################################################
 
+#--- network related methods -------------------------------
 def getDnsEntry(ip):
         """ get the dns name for a given ip address """
         try:
@@ -26,4 +27,37 @@ def isSmbServer(ip):
         except:
             return False
 
+def ipToInt(IP):
+    ''' 255.255.255.255 => 4294967295 '''
+    IntIP=0
+    count=3
+    for i in IP.split("."):
+        if i!='0':
+            IntIP+=int(i)*256**count
+        count-=1           
+    return IntIP
+
+def intToIp(IntIP):
+    IP=""
+    for i in [(int(IntIP) & item[0]) >> 8*item[1] for item in [[255 << 8*k,k] for k in xrange(3,-1,-1)]]:
+        IP+=str(i)+"."
+    return IP[:-1]
+
+
+#--- file related methods --------------------------
 	
+def makePretty(value):
+    ''' convert bit values in human readable form '''
+    steps = [ (1024,"KiB"), (1048576, "MiB"), (1073741824, "GiB"), (1099511627776, "TiB") ]
+    for step in steps:
+        m = step[0]
+        suffix = step[1]
+        try:
+            cs = value / m
+        except:
+            return ""
+        if cs < 10:
+            return "%3.1f" % (cs) + ' ' + suffix
+        if cs < 1024:
+            return unicode( int(cs) ) + ' ' + suffix
+    return "very big"
