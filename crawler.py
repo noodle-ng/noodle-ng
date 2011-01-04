ea065421c8a921b6065ad2132235b11493ca5623
Christian Hundt <hundt.christian@googlemail.com>
2011-01-04 22:24:15 +0000

    deleted trash from crawler.py for new pysmbc crawler
    git-svn-id: https://noodle-ng.googlecode.com/svn/trunk@67 8aa6f4f4-6411-944d-c798-68ba1b83f409

--- crawler.py
+++ crawler.py
@@ -1,20 +1,4 @@
-
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# smb and dns methods
-import noodleLib as n
-
-
-# getting database url from production.ini
-try:
-    import ConfigParser
-    config = ConfigParser.SafeConfigParser()
-    #config.read('development.ini')
-    config.read('production.ini')
-    url = config.get('app:main','sqlalchemy.url',raw=True)
-    if not url: raise
-except:
-    url = 'sqlite:///%(here)s/somedb.db'
-
-print n.getDnsEntry('google.de') , n.isSmbServer('google.de')
+# this will be the new crawler
