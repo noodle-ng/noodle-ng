b32eeffd27d7213b326f9a8b08b76661bf2d32c7
Christian Hundt <hundt.christian@googlemail.com>
2011-01-05 00:22:47 +0000

    header for crawler.py written - feel free to insert comments and answer given questions
    git-svn-id: https://noodle-ng.googlecode.com/svn/trunk@68 8aa6f4f4-6411-944d-c798-68ba1b83f409

--- crawler.py
+++ crawler.py
@@ -2,3 +2,53 @@
 # -*- coding: utf-8 -*-
 
 # this will be the new crawler
+################################################################################
+# global configuration which  should be definied somewhere else in the future
+################################################################################
+
+#login credentials
+credentials = {'Gast':'123Dabei','anonymous':''}
+
+################################################################################
+# local configuration which can stay here
+################################################################################
+
+# smbc type shortcuts
+smbc_type={'share':3,'folder':7,'file':8}
+
+################################################################################
+# functions which should be sourced out to somewhere else in the future
+################################################################################
+
+def getDnsEntry(ip):
+    """ get the dns name for a given ip address """
+    
+    # !!! EDIT THIS !!!
+    # should this import be local or global in the future?
+    import socket as sk
+    
+    try:
+        entry = sk.gethostbyaddr(ip)[0]
+    except:
+        entry = None
+    return entry
+
+def splitFileName(s):
+    """ splits Kobe.avi to 'Kobe' and 'avi' """
+    
+    name=''
+    ext=''
+    
+    #reverse fileName s
+    s=s[::-1]
+    #if no dot in fileName s
+    position=s.find('.')
+    
+    #if no dot in fileName s
+    if position == -1 :
+        name=s[::-1]
+    #else split by dot
+    else:
+        ext=s[:position][::-1]
+        name=s[position+1:][::-1]
+    return [name,ext]
