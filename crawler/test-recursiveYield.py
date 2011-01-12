11e71d6c3653d0d3ffa346cb6fb3e1e7b40f0f6b
Christian Hundt <hundt.christian@googlemail.com>
2011-01-12 00:20:06 +0000

    minor modifications at test-recursiveYield.py
    git-svn-id: https://noodle-ng.googlecode.com/svn/trunk@79 8aa6f4f4-6411-944d-c798-68ba1b83f409

--- crawler/test-recursiveYield.py
+++ crawler/test-recursiveYield.py
@@ -5,13 +5,25 @@ def faculty(k,result=1,counter=1):
         return result*counter
     else:
         return faculty(k,result*counter,counter+1)
-    
+
+#crap   
 def facultyGenerator(k,result=1,counter=1):
     
     if counter == k:
+        yield result*counter
         raise StopIteration
     else:
-        yield result*counter
-        yield faculty(k,result*counter,counter+1)
+        yield result
+        yield facultyGenerator(k,result*counter,counter+1).next()
 
-print list(facultyGenerator(4))
+def count():
+    yield 1
+    yield 2
+    yield 3
+    raise StopIteration
+
+print list(count())
+
+
+#print faculty(4)
+#print list(facultyGenerator(4))
