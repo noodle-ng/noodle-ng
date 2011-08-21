abe8728b5c0d8c90a086e047a657e2324edae3a5
Moritz Schlarb <mail@moritz-schlarb.de>
2011-08-21 00:25:39 +0000

    First part of new crawler implementation. Just config file parsing and setting up Pools and workers. Please note that the syntax of crawler.ini has slightly changed in order to provide a more safe parsing mechanism (eval is evil...)
    git-svn-id: https://noodle-ng.googlecode.com/svn/trunk@131 8aa6f4f4-6411-944d-c798-68ba1b83f409

--- crawler.py
+++ crawler.py
@@ -1,3 +1,83 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+This it the Noodle NG file crawler main module.
 
+It is executable and will read it's configuration file and then
+begin working based on the information it finds there.
+
+"""
+
+import sys, os
+import logging
+from multiprocessing import Pool
+from ConfigParser import SafeConfigParser
+
+from noodle.lib.iptools import IpRange
+
+# Some constant values
+config_file = "crawler.ini"
+
+def setup_worker(type):
+    """Sets up a worker process"""
+    logging.debug("setting up worker for %s" % type)
+    return
+
+def crawl(host):
+    """Starts the crawling process for one host"""
+    logging.debug("crawling %s" % host)
+    return
+
+def main():
+    """Runs the crawler"""
+    
+    # Parsing the configuration
+    
+    config = SafeConfigParser({'here': sys.path[0]})
+    try:
+        config.read(os.path.join(sys.path[0], config_file))
+    except:
+        sys.exit("Could not read %s" % config_file)
+    
+    debug = config.getboolean('main', 'debug')
+    if debug:
+        logging.basicConfig(level=logging.DEBUG)
+    processes = config.getint('main', 'processes')
+    
+    sqlalchemy_url = config.get('main', 'sqlalchemy.url')
+    sqlalchemy_echo = config.getboolean('main', 'sqlalchemy.echo')
+    
+    locations = []
+    
+    for name in [section for section in config.sections() if section != 'main']:
+        
+        location = {}
+        location['name'] = name
+        location['type'] = config.get(name, 'type')
+        location['hosts'] = []
+        for element in config.get(name, 'hosts').split(','):
+            element = element.strip()
+            if element.find('-') != -1:
+                # IP range
+                start, stop = element.split('-',1)
+                location['hosts'].append(IpRange(start.strip(), stop.strip()))
+            else:
+                # CIDR range or single IP
+                location['hosts'].append(IpRange(element))
+        location['credentials'] = []
+        for cred in config.get(name, 'credentials').split(','):
+            location['credentials'].append(tuple(cred.strip().split(':')))
+        locations.append(location)
+    logging.debug(locations)
+    
+    for location in locations:
+        pool = Pool(processes, setup_worker, (location['type'],))
+        for hosts in location['hosts']:
+            pool.map_async(crawl, hosts)
+        pool.close()
+        pool.join()
+    
+    return
+
+if __name__ == '__main__':
+    main()
